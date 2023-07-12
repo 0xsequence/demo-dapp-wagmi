f3133f31e59f805e2846aaf3b50ff14f0454160b
attente <william@attente.ca>
2023-07-12 19:55:50 +0000

    Build: (ad11fb2) bump sequence.js to 1.0.0, wagmi-connector to 2.0.1

--- assets/index-f35306b4.js
+++ assets/index-f35306b4.js
@@ -1,4 +1,4 @@
-import{a as mt,H as At,s as J,K as D,C as Z,d as T,b as S,n as W,c as O,o as G,y as z,_ as H}from"./index-fb343fc6.js";import"./index-c4e0238d.js";/**
+import{a as mt,H as At,s as J,K as D,C as Z,d as T,b as S,n as W,c as O,o as G,y as z,_ as H}from"./index-e5c93292.js";import"./index-732443e7.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause

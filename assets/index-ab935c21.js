6a297554907e2e2a5ee69040028d4d828c1a6e94
Agusx1211 <agusxrun@gmail.com>
2023-07-28 16:01:21 +0000

    Build: (a2d1e9f) Merge pull request #5 from 0xsequence/sequence-v1.1.0
    Use Sequence v1.1.0

--- assets/index-ab935c21.js
+++ assets/index-ab935c21.js
@@ -1,4 +1,4 @@
-import{a as mt,H as At,s as J,K as D,C as Z,d as T,b as S,n as W,c as O,o as G,y as z,_ as H}from"./index-8ad2ce7d.js";import"./index-fd7f278b.js";/**
+import{a as mt,H as At,s as J,K as D,C as Z,d as T,b as S,n as W,c as O,o as G,y as z,_ as H}from"./index-701974f3.js";import"./index-598e891c.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause

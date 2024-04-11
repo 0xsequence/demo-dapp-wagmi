bcabfde168d08e955f46608ec09d68761cd0d6cc
Corban Riley <corbanbrook@gmail.com>
2024-04-11 11:19:23 -0400

    Updating seqeunce to v1.9.17, removing mumbai and adding amoy and arbitrum sepolia

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,59 @@
 import { ThemeProvider } from '@0xsequence/design-system'
 
 import { sequenceWallet } from '@0xsequence/wagmi-connector'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query' 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { walletConnect } from 'wagmi/connectors'
 
-import {
-  createConfig,
-  WagmiProvider,
-  http
-} from 'wagmi';
+import { createConfig, WagmiProvider, http } from 'wagmi'
 import { mainnet, polygon, optimism, arbitrum, polygonMumbai, sepolia, Chain } from '@wagmi/chains'
 import { sequence } from '0xsequence'
 import Demo from './Demo'
 
+import './index.css'
 import '@0xsequence/design-system/styles.css'
 
-const queryClient = new QueryClient() 
+const queryClient = new QueryClient()
+
+const arbitrumSepolia: Chain = {
+  id: 421614,
+  name: 'Arbitrum Sepolia',
+  network: 'aribtrum-sepolia',
+  testnet: true,
+  nativeCurrency: {
+    name: 'Arbitrum Sepolia Ether',
+    symbol: 'ASETH',
+    decimals: 18
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://nodes.sequence.app/arbitrum-sepolia']
+    },
+    public: {
+      http: ['https://nodes.sequence.app/arbitrum-sepolia']
+    }
+  }
+}
+
+const polygonAmoy: Chain = {
+  id: 80002,
+  name: 'Polygon Amoy',
+  network: 'polygon-amoy',
+  testnet: true,
+  nativeCurrency: {
+    name: 'Matic Amoy',
+    symbol: 'MATIC',
+    decimals: 18
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://nodes.sequence.app/amoy']
+    },
+    public: {
+      http: ['https://nodes.sequence.app/amoy']
+    }
+  }
+}
 
 const App = () => {
   const urlParams = new URLSearchParams(window.location.search)
@@ -26,7 +63,7 @@ const App = () => {
     walletAppURL = urlParams.get('walletAppURL')
   }
 
-  const chains = [mainnet, polygon, optimism, arbitrum, polygonMumbai, sepolia] as [Chain, ...Chain[]]
+  const chains = [mainnet, polygon, optimism, arbitrum, sepolia, polygonAmoy, arbitrumSepolia] as [Chain, ...Chain[]]
 
   const connectors = [
     sequenceWallet({
@@ -38,15 +75,15 @@ const App = () => {
         // This is optional, and only used to point to a custom
         // environment for the wallet app. By default, it will
         // point to https://sequence.app/
-        walletAppURL,
+        walletAppURL
       }
     }),
     walletConnect({
       projectId: 'b87cf8b78e1c5a9881adabe5765d2461',
-      showQrModal: true,
-    }),
+      showQrModal: true
+    })
   ]
-  
+
   const transports = {}
 
   chains.forEach(chain => {
@@ -58,15 +95,15 @@ const App = () => {
   const wagmiConfig = createConfig({
     chains,
     connectors,
-    transports,
+    transports
   })
 
   return (
     <ThemeProvider>
       <WagmiProvider config={wagmiConfig}>
-        <QueryClientProvider client={queryClient}> 
+        <QueryClientProvider client={queryClient}>
           <Demo />
-        </QueryClientProvider> 
+        </QueryClientProvider>
       </WagmiProvider>
     </ThemeProvider>
   )
